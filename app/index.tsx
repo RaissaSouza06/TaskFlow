d3b1bd9499ad381c73371413eefa1d0783fe037e
Raissa de Oliveira Souza <rosouza_23@outlook.com>
2026-08-11 22:50:44 -0300

    feat: primeiro commit do projeto

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,8 @@ import {Text, View} from 'react-native'
 export default function Home() {
     return (
         <View>
-            <Text>
-                Olá mundo!
-            </Text>
+            <Text>Olá mundo!</Text>
+            <Text>Hello word!</Text>
         </View>
     );
 }
